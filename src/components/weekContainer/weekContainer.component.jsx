1672243b1c733d0b518ebe54f0ef28ddbf555de3
taylor <taylor@affair.dev>
2020-07-13 23:58:07 -0400

    finished app

--- src/components/weekContainer/weekContainer.component.jsx
+++ src/components/weekContainer/weekContainer.component.jsx
@@ -25,14 +25,27 @@ class WeekContainer extends React.Component {
         this.setState({search: e.target.value});
     }
 
-    callApi = () => {
-        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.state.search},us&units=imperial&APPID=${config.openWeatherMapKey}`)
-            .then(res => res.json())
-            .then(data => {
-                const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
-                this.setState({days: dailyData, city: data.city.name})
 
-            })
+    callApi = () => {
+        let zipCodePattern = /^\d{5}$|^\d{5}-\d{4}$/;
+
+        let search = this.state.search;
+
+        if (zipCodePattern.test(search)) {
+            fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${this.state.search},us&units=imperial&APPID=${config.openWeatherMapKey}`)
+                .then(res => res.json())
+                .then(data => {
+                    const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+                    this.setState({days: dailyData, city: data.city.name})
+                })
+        } else {
+            console.error(`Incorrect zipcode entered`);
+        }
+    }
+    
+    handleSubmit = e => {
+        e.preventDefault();
+        this.callApi();
     }
 
     formatCards = () => {
@@ -42,19 +55,21 @@ class WeekContainer extends React.Component {
     render() {
         return (
             <div className="container">
-                <h1 className="display-3 jumbotron">5-Day Forecast for {this.state.city}.</h1>
+                <h1 className="display-3 jumbotron">5-Day Forecast for {this.state.city}</h1>
 
                 <div className="row justify-content-center">
                     <div className="col-md-6">
                         <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Search by city or zip"
-                                   onChange={this.handleChange}
-                                   aria-label="Search by city or zip" aria-describedby="button-addon2" />
-                                <div className="input-group-append">
-                                    <button className="btn btn-outline-secondary" onSubmit={this.callApi} type="button"
+                            <form className="form-inline" onSubmit={this.handleSubmit}>
+                                <div className="form-group m-3">
+                                    <input type="text" id="search" className="form-control" placeholder="Search by zip"
+                                           onChange={this.handleChange}
+                                           aria-label="Search by zip" aria-describedby="button-addon2"/>
+                                </div>
+                                    <button className="btn btn-outline-secondary" type="submit"
                                             id="button-addon2">Search
                                     </button>
-                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
